fda6fe79014a330f790faf97ca39393e80c482b6
parKsungjea <sungjea15@naver.com>
2024-03-05 17:36:59 +0900

    i "asdsad "

--- src/main/java/com/winter/app/board/BoardDAO.java
+++ src/main/java/com/winter/app/board/BoardDAO.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface BoardDAO {
-
+    
 	public List<BoardVO> getList()throws Exception;					
 	
 	public int add(BoardVO boardVO) throws Exception;
