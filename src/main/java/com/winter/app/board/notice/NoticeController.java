c30e4822e2f7bcd74ffa2d773167b74c7ba424c7
parKsungjea <sungjea15@naver.com>
2024-03-13 14:05:29 +0900

    12321213

--- src/main/java/com/winter/app/board/notice/NoticeController.java
+++ src/main/java/com/winter/app/board/notice/NoticeController.java
@@ -2,22 +2,36 @@ package com.winter.app.board.notice;
 
 import java.util.List;
 
+import javax.naming.Binding;
+
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.winter.app.board.BoardVO;
 import com.winter.app.util.Pager;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/notice/*")
+@Slf4j
 public class NoticeController {
 	
 	@Autowired
 	private NoticeService noticeService;
+	
+	@Value("${board.notice.name}")
+	private String name;
 	
 	@GetMapping("list")
 	public String getList(Pager pager, Model model)throws Exception{
@@ -36,6 +50,12 @@ public class NoticeController {
 	public String add(NoticeVO noticeVO)throws Exception{
 		int result = noticeService.add(noticeVO);
 		return "redirect:./list";
+	}
+	
+	@GetMapping("create")
+	public String create(@ModelAttribute BoardVO boardVO)throws Exception {
+		
+		return "board/create";
 	}
 
 }
